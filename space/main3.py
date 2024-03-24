886b80303325f1a61ce1febaa2e423faed294867
Quilonis <quilonis001@gmail.com>
2024-03-24 14:00:33 +0200

    123457

---
+++ space/main3.py
@@ -0,0 +1,286 @@
+from pygame import *
+from random import randint
+import os
+
+font.init()
+font1 = font.Font(None, 80)
+win = font1.render('YOU WIN!', True, (255, 255, 255))
+lose = font1.render('YOU LOSE!', True, (180, 0, 0))
+
+font2 = font.Font(None, 36)
+
+mixer.init()
+mixer.music.load(r'Assets\space.ogg')
+mixer.music.play()
+fire_sound = mixer.Sound(r'Assets\fire.ogg')
+
+img_back = r"Assets\galaxy.jpg"
+
+img_bullet = [r"Assets\bullet.png", r'Assets\ShotGun_bullet.png', r"Assets\star_bullet.png",r"Assets\shot_gun_bullet.png"]
+boxes = [r"Assets\ShotGun_Bullet_box.png", r"Assets\star_box.png", r"Assets/size_box.png", r"Assets\Box_shotgun.png"]
+
+
+
+
+img_hero = r"Assets\rocket.png"
+img_enemy = r"Assets\ufo.png"
+
+
+
+score = 0
+goal = 500
+lost = 0
+max_lost = 10
+workdir = os.getcwd()
+
+
+class GameSprite(sprite.Sprite):
+
+    def __init__(self, image_, width, height, x, y):
+        sprite.Sprite.__init__(self)
+
+        self.image = transform.scale(image.load(os.path.join(image_)), (width, height))
+
+        self.rect = self.image.get_rect()
+
+        self.rect.x = x
+        self.rect.y = y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+
+    def __init__(self, image, width, height, x, y):
+        super().__init__(image, width, height, x, y)
+
+        self.speed = 20
+
+        self.current_bullet = "standart"
+
+        self.upgrade_timer = 0
+
+    def update(self):
+        keys = key.get_pressed()
+        if self.current_bullet != "standart":
+
+            if self.upgrade_timer <= 3:
+                self.upgrade_timer += 0.05
+
+            else:
+                self.upgrade_timer = 0
+
+                self.current_bullet = 0
+
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+
+    def fire(self):
+        bullet = Bullet(img_bullet[self.current_bullet], 9, 21, self)
+        bullets.add(bullet)
+
+
+class Enemy(GameSprite):
+    def __init__(self, image, width, height, x, y):
+        super().__init__(image, width, height, x, y)
+        self.speed = randint(1, 5)
+
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_width - 80)
+            self.rect.y = 0
+            lost = lost + 1
+
+
+class Bullet(GameSprite):
+    def __init__(self, image, width, height, player):
+        super().__init__(image, width, height, player.rect.x + 21, player.rect.y)
+
+
+        self.bullet = player.current_bullet
+
+
+        if self.bullet == "standart":
+            self.speed = 15
+            self.image = transform.scale(image.load(os.path.join(r"Assets\bullet.png")), (width, height))
+
+        elif self.bullet == "shot_gun":
+            self.speed = 5
+            self.image = transform.scale(image.load(os.path.join(r'Assets\ShotGun_bullet.png')), (width, height))
+
+        elif self.bullet == "star":
+            self.speed = 50
+            self.image = transform.scale(image.load(os.path.join(r"Assets\star_bullet.png")), (width, height))
+
+
+    def update(self):
+        self.rect.y -= self.speed
+
+        global bullets
+        global monsters
+        global score
+
+        collides = sprite.groupcollide(monsters, bullets, True, True)
+        if collides:
+            if self.bullet == 1:
+                bullet = SBullet(r"Assets\Steel_ball.png", 25, 25, self.rect.x, self.rect.y, 2, 0)
+                bullets.add(bullet)
+                bullet = SBullet(r"Assets\Steel_ball.png", 25, 25, self.rect.x, self.rect.y, -2, 0)
+                bullets.add(bullet)
+            if self.bullet == 2:
+                bullet = SBullet(r"Assets\star.png", 25, 25, self.rect.x, self.rect.y, 0, -2)
+                bullets.add(bullet)
+        for c in collides:
+            score = score + 1
+            monster = Enemy(img_enemy, 50, 50, randint(20, 600), -20)
+            monsters.add(monster)
+
+        if self.rect.y < 0:
+            self.kill()
+
+
+class SBullet(GameSprite):
+    def __init__(self, image_, width, height, x, y, dx, dy):
+        super().__init__(image_, width, height, x, y)
+        self.dx = dx
+        self.dy = dy
+
+    def update(self):
+        self.rect.x += self.dx
+        self.rect.y += self.dy
+
+        global ship
+        global monsters
+        global score
+
+        if ship.current_bullet == 2:
+            collides = sprite.groupcollide(monsters, bullets, True, False)
+        else:
+            collides = sprite.groupcollide(monsters, bullets, True, True)
+        for c in collides:
+            score = score + 1
+            monster = Enemy(img_enemy, 50, 50, randint(20, 600), -20)
+            monsters.add(monster)
+
+        if self.rect.y < 0 and self.rect.y > 500 and self.rect.x > 700 and self.rect.x < 0:
+            self.kill()
+
+
+class Power_box(GameSprite):
+    def __init__(self, image_, width, height, x, y, type):
+        super().__init__(image_, width, height, x, y)
+
+        self.type = type
+        self.speed = 2
+
+    def update(self):
+        global ship
+        global power_boxes
+
+        self.rect.y += self.speed
+
+        collides = sprite.spritecollide(ship, power_boxes, True)
+
+        for c in collides:
+            ship.current_bullet = self.type
+        if self.rect.y > 500:
+            self.kill()
+
+
+
+box_classes = [Power_box(r"Assets\ShotGun_Bullet_box.png",40,40,0,0,"shot_gun"),
+               Power_box(r"Assets\star_box.png",40,40,0,0,"star"),
+               Power_box(r"Assets\size_box.png",40,40,0,0,"size"),
+               Power_box(r"Assets\Electric_box.png",40,40,0,0,"electric")]
+
+win_width = 700
+win_height = 500
+display.set_caption("Shooter")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
+
+ship = Player(img_hero, 50, 50, 50, 390)
+
+monsters = sprite.Group()
+for i in range(0, 6):
+    monster = Enemy(img_enemy, 50, 50, randint(20, 600), -20)
+    monsters.add(monster)
+print(monsters)
+
+power_boxes = sprite.Group()
+
+bullets = sprite.Group()
+
+finish = False
+
+run = True
+
+timer_1 = 0
+
+while run:
+
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
+
+    if not finish:
+
+        if timer_1 <= 5:
+            timer_1 += 0.05
+        else:
+            print("END!")
+
+            type = randint(0, len(boxes) - 1)
+            box = Power_box(boxes[type], 40, 40, randint(40, 640), -20, type)
+            power_boxes.add(box)
+
+            timer_1 = 0
+
+        window.blit(background, (0, 0))
+
+        text = font2.render("Score: " + str(score), 1, (255, 255, 255))
+        window.blit(text, (10, 20))
+
+        text_lose = font2.render("Missed: " + str(lost), 1, (255, 255, 255))
+        window.blit(text_lose, (10, 50))
+
+        ship.update()
+        monsters.update()
+        bullets.update()
+        power_boxes.update()
+
+        ship.reset()
+        monsters.draw(window)
+        bullets.draw(window)
+
+        power_boxes.draw(window)
+
+        collides = sprite.groupcollide(monsters, bullets, True, True)
+
+        for c in collides:
+            score = score + 1
+            monster = Enemy(img_enemy, 50, 50, randint(20, 600), -20)
+            monsters.add(monster)
+
+        if sprite.spritecollide(ship, monsters, False) or lost >= max_lost:
+            finish = True  # проиграли, ставим фон и больше не управляем спрайтами.
+            window.blit(lose, (200, 200))
+
+        if score >= goal:
+            finish = True
+            window.blit(win, (200, 200))
+
+        display.update()
+
+    time.delay(50)
